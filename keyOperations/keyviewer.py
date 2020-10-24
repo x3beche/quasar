8eb3d485e019af7e38a849471259599fd0c7ea35
Emirhan <53257417+x3beche@users.noreply.github.com>
2020-10-24 11:05:33 +0300

    update

--- keyOperations/keyviewer.py
+++ keyOperations/keyviewer.py
@@ -1,8 +1,11 @@
-while True:
-    data=input("Enter the number at the end of the key file : ")
-
+import os
+f=open("banner", "r")
+print(f.read())
+def opnr(data):
     f = open("key"+str(data), "r")
     klst=f.read().split(",")
-
     for x in range(0,len(klst)):
         print(klst[x])
+data=input("Enter the number at the end of the key file : ")
+os.system('cls' if os.name == 'nt' else 'clear')
+opnr(data)
