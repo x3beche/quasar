17c19b4bb82cf0b97a64142221884dd24ac91a3f
x3beche <emirpehlevan@gmail.com>
2020-10-25 13:39:23 +0300

    some improvements

--- encrypt.py
+++ encrypt.py
@@ -13,17 +13,24 @@ banner()
 keynum=input("Enter the number at the end of the key file : ")
 clear()
 fn="keyOperations\\key"+keynum
-f=open(fn,"r")
-klst=f.read().split(",")
-f.close()
-a=klst[0].split("-")
-rtry=[[],[]]
-for x in range(0,26):
-    cache=klst[x].split("-")
-    rtry[0].append(cache[0])
-    rtry[1].append(cache[1])
-    exec(rtry[1][x]+"=rtry[0][x]")
-    cache.clear()
+fc = os.path.isfile(fn)
+if fc==True:
+    f=open(fn,"r")
+    klst=f.read().split(",")
+    f.close()
+    a=klst[0].split("-")
+    rtry=[[],[]]
+    for x in range(0,26):
+        cache=klst[x].split("-")
+        rtry[0].append(cache[0])
+        rtry[1].append(cache[1])
+        exec(rtry[1][x]+"=rtry[0][x]")
+        cache.clear()
+else:
+    clear()
+    banner()
+    print("Key file not found, make sure key file is in keyOperations directory.")
+    exit()
 
 #encrypt algorithm
 oW=rtry[1][ord(a[1])-65]
@@ -39,21 +46,22 @@ def call(oW,nW,rtry):
 
 #ui start
 banner()
-print("\n1-) Encrypt file (only txt files support)\n2-) Encrypt normal text\n")
+print("1-) Encrypt file (only txt files support)\n2-) Encrypt normal text\n")
 obs=input("Option : ")
 if int(obs)==1:
     pass
 elif int(obs)==2:
     clear()
     banner()
-    print("\nWrite text\n")
-    text=input("->")
+    text=input("Text -> ")
+    text=text.upper()
+    text=text.replace(" ","")
     for y in range(0,len(text)):
         #final.append(call(oW,text[y],rtry))
         oW=call(oW,text[y],rtry)
         final=final+oW
     clear()
     banner()
-    print("\nNormal text : ",text,"\nEncrypted text : ",final)
+    print("Normal text:",text,"\nEncrypted text:",final)
 else:
     print("Wrong option.")
