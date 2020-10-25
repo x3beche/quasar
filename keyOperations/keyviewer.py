ddf6dd6022405436549d3efce2bd7d2d5e073eab
x3beche <emirpehlevan@gmail.com>
2020-10-25 15:09:48 +0300

    viewer improve

--- keyOperations/keyviewer.py
+++ keyOperations/keyviewer.py
@@ -1,13 +1,12 @@
-import os
 f=open("banner", "r")
 print(f.read())
 f.close()
 def opnr(data):
+    print("\n")
     f = open("key"+str(data), "r")
     klst=f.read().split(",")
     for x in range(0,len(klst)):
         print(klst[x])
     f.close()
 data=input("Enter the number at the end of the key file : ")
-os.system('cls' if os.name == 'nt' else 'clear')
 opnr(data)
