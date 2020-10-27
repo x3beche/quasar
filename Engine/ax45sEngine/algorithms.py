88ab3328f8ce005f0464726da704ac95dbc2907d
x3beche <emirpehlevan@gmail.com>
2020-10-27 22:34:40 +0300

    AX45-S Engine added

---
+++ Engine/ax45sEngine/algorithms.py
@@ -0,0 +1,55 @@
+def f_encrypt(oW,nW,rtry):
+    own=int(rtry[0][rtry[1].index(oW)])
+    nwn=int(rtry[0][rtry[1].index(nW)])
+    if own>nwn:
+        chc=94-own+nwn
+    else:
+        chc=nwn-own
+    return rtry[1][chc-1]
+
+def f_decrypt(oW,nW,rtry):
+    own=int(rtry[0][rtry[1].index(oW)])
+    nwn=int(rtry[0][rtry[1].index(nW)])
+    if own+nwn>94:
+        chc=own+nwn-94
+    else:
+        chc=nwn+own
+    return rtry[1][chc-1]
+
+def keyNameComplier(keyNumber):
+    keyName="key"+str(keyNumber)+".ax"
+    return keyName
+
+def keyComplier(fn):
+    f=open(fn,"r")
+    klst=f.read().replace("\n","").split("split")
+    f.close()
+    a=klst[0].split("axen")
+    rtry=[[],[]]
+    for x in range(0,94):
+        cache=klst[x].split("axen")
+        rtry[0].append(cache[0])
+        rtry[1].append(cache[1])
+        cache.clear()
+    return rtry,a
+
+def axen(text,keyNumber):
+    a=keyComplier(keyNameComplier(keyNumber))[1]
+    rtry=keyComplier(keyNameComplier(keyNumber))[0]
+    oW=rtry[1][ord(a[1])]
+    final=''
+    for y in range(0,len(text)):
+        oW=f_encrypt(oW,text[y],rtry)
+        final=final+oW
+    return final
+
+def axde(text,keyNumber):
+    a=keyComplier(keyNameComplier(keyNumber))[1]
+    rtry=keyComplier(keyNameComplier(keyNumber))[0]
+    oW=rtry[1][ord(a[1])]
+    final=""
+    for y in range(0,len(text)):
+        nW=text[y]
+        final=final+f_decrypt(oW,nW,rtry)
+        oW=text[y]
+    return final
---
+++ Engine/test.py
@@ -0,0 +1,7 @@
+from ax45sEngine.algorithms import axen,axde
+
+text="AX45-S Crypto Algorithm"
+encrypted=axen(text,1001)
+
+print(axen(text,1001))
+print(axde(encrypted,1001))
