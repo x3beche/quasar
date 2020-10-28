81a96fd70595cc8f7505fde07b17cb86ff0ee7f8
x3beche <emirpehlevan@gmail.com>
2020-10-28 12:49:50 +0300

    Test script improvment

--- Engine/test.py
+++ Engine/test.py
@@ -3,5 +3,5 @@ from ax45sEngine.algorithms import axen,axde
 text="AX45-S Crypto Algorithm"
 encrypted=axen(text,1001)
 
-print(axen(text,1001))
-print(axde(encrypted,1001))
+print("This encrypt state : ",axen(text,1001))
+print("This decrypt state : ",axde(encrypted,1001))
