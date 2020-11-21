0dfb8cc704cddb699a3c28caa0850ed41291104a
x3beche <emirpehlevan@gmail.com>
2020-11-21 12:30:23 +0300

    key update

--- python/Engine/test.py
+++ python/Engine/test.py
@@ -3,5 +3,5 @@ from ax45sEngine.algorithms import axen,axde
 text="sasasasasasasasasasasasasa"
 encrypted=axen(text,1001)
 
-print("This encrypt state : ",axen(text,1001))
-print("This decrypt state : ",axde(encrypted,1001))
+print("This encrypt state : ",axen(text,237415))
+print("This decrypt state : ",axde(encrypted,237415))
