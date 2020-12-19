de9f8db28c1804434723eebb185993be29caee33
x3beche <emirpehlevan@gmail.com>
2020-12-19 13:48:21 +0300

    key integer

--- python/Engine/test.py
+++ python/Engine/test.py
@@ -1,7 +1,8 @@
 from ax45sEngine.algorithms import axen,axde
 
+key=237415
 text='test >< +-*/ crypto'
-encrypted=axen(text,237415)
 
-print('This encrypt state : "'+axen(text,237415)+'"')
-print('This decrypt state : "'+axde(encrypted,237415)+'"')
+encrypted=axen(text,key)
+print('This encrypt state : "'+axen(text,key)+'"')
+print('This decrypt state : "'+axde(encrypted,key)+'"')
