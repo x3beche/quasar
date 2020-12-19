74aeb4856c2669608b6203bef25609d8bb5482d1
x3beche <emirpehlevan@gmail.com>
2020-12-19 13:37:14 +0300

    improvment

--- python/Engine/test.py
+++ python/Engine/test.py
@@ -1,7 +1,7 @@
 from ax45sEngine.algorithms import axen,axde
 
-text="sasasasasasasasasasasasasa"
-encrypted=axen(text,1001)
+text='test >< +-*/ crypto'
+encrypted=axen(text,237415)
 
-print("This encrypt state : ",axen(text,1001))
-print("This decrypt state : ",axde(encrypted,1001))
+print('This encrypt state : "'+axen(text,237415)+'"')
+print('This decrypt state : "'+axde(encrypted,237415)+'"')
